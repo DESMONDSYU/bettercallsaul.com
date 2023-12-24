91355c288316786ce9aa256f0aa0a6c37e6270e4
DESMONDSYU <142507179+DESMONDSYU@users.noreply.github.com>
2023-12-23 22:16:20 -0500

    Add files via upload

---
+++ finalproject/Script/staff.js
@@ -0,0 +1,65 @@
+function toggleContent1(){
+    var dropdownContent = document.getElementById("readmore-saul");
+    var foldContent = document.getElementById("foldbtn1");
+    var dropContent = document.getElementById("readmorebtn1");
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === ""){
+        dropdownContent.style.display = "block";
+        foldContent.style.display = "block";
+        dropContent.style.display = "none";
+    } 
+}
+
+function foldContent1(){
+    var dropdownContent = document.getElementById("readmore-saul");
+    var foldContent = document.getElementById("foldbtn1");
+    var dropContent = document.getElementById("readmorebtn1");
+    if (dropdownContent.style.display === "block"){
+        dropdownContent.style.display = "none";
+        foldContent.style.display = "none";
+        dropContent.style.display = "block";
+    } 
+}
+
+function toggleContent2(){
+    var dropdownContent = document.getElementById("readmore-fran");
+    var foldContent = document.getElementById("foldbtn2");
+    var dropContent = document.getElementById("readmorebtn2");
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === ""){
+        dropdownContent.style.display = "block";
+        foldContent.style.display = "block";
+        dropContent.style.display = "none";
+    } 
+}
+
+function foldContent2(){
+    var dropdownContent = document.getElementById("readmore-fran");
+    var foldContent = document.getElementById("foldbtn2");
+    var dropContent = document.getElementById("readmorebtn2");
+    if (dropdownContent.style.display === "block"){
+        dropdownContent.style.display = "none";
+        foldContent.style.display = "none";
+        dropContent.style.display = "block";
+    } 
+}
+
+function toggleContent3(){
+    var dropdownContent = document.getElementById("readmore-huell");
+    var foldContent = document.getElementById("foldbtn3");
+    var dropContent = document.getElementById("readmorebtn3");
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === ""){
+        dropdownContent.style.display = "block";
+        foldContent.style.display = "block";
+        dropContent.style.display = "none";
+    } 
+}
+
+function foldContent3(){
+    var dropdownContent = document.getElementById("readmore-huell");
+    var foldContent = document.getElementById("foldbtn3");
+    var dropContent = document.getElementById("readmorebtn3");
+    if (dropdownContent.style.display === "block"){
+        dropdownContent.style.display = "none";
+        foldContent.style.display = "none";
+        dropContent.style.display = "block";
+    } 
+}
